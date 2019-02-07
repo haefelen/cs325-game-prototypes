157e405d183fe1a4d831b0d8e57099f7e08a5676
haefelen <47426197+haefelen@users.noreply.github.com>
2019-02-07 14:11:34 -0500

    Add files via upload

--- HW1/js/main.js
+++ HW1/js/main.js
@@ -1,49 +1,92 @@
-"use strict";
-
-window.onload = function() {
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
+var game = new Phaser.Game(852, 839, Phaser.AUTO, '', {preload: preload, create: create, update: update});
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
+    var player;
+    var cursor;
+    var rock;
+    var counter = 0;
+    var text = 0;
+    var text2 = 0;
+    var scoreLoop;
+    var rockLoop;
     
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+    function preload(){
+        game.load.image('ship', 'v1assets/ship1.png');
+        game.load.image('rock', 'v1assets/rock1.png');
     }
     
-    var bouncy;
-    
-    function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
+    function create(){
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
+        scoreLoop = game.time.events.loop(Phaser.Timer.SECOND, score, this);        
+        rockLoop = game.time.events.loop(Phaser.Timer.SECOND*1.5, spawnRock, this);
         
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+        text2 = game.add.text(175, 50, 'Score: 0', { font: "64px Arial", fill: "#ffffff", align: "left" });
+        text2.anchor.setTo(0.5, 0.5);
+        
+        player = game.add.sprite(426, 819, 'ship');
+        player.anchor.setTo(0.5, 0.5);
+        game.physics.arcade.enable(player, Phaser.Physics.ARCADE);
+        player.body.drag.set(100);
+        player.body.maxVelocity.set(200);
+        player.body.collideWorldBounds = true;
+        
+        cursor = game.input.keyboard.createCursorKeys();
+        
+        rocks = game.add.group();
+        game.physics.arcade.enable(rocks, Phaser.Physics.ARCADE);
+        rocks.enableBody = true;        
     }
     
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
+    function update(){
+        
+        if (cursor.up.isDown){
+        game.physics.arcade.accelerationFromRotation(player.rotation-(Math.PI/2), 200, player.body.acceleration);
+        }
+        else {
+            player.body.acceleration.set(0);
+        }
+        if (cursor.right.isDown){
+            player.body.angularVelocity = 300;
+        }
+        else if (cursor.left.isDown){
+            player.body.angularVelocity = -300;
+        }
+        else{
+            player.body.angularVelocity = 0;
+        }
+        
+        game.physics.arcade.overlap(player, rocks, gameOver, null, this);
+        game.physics.arcade.collide(rocks, rocks, killRock, null, this);
     }
-};
+    
+    function spawnRock(){
+        rock = rocks.create(426, 81, 'rock');
+        rock.anchor.setTo(0.5, 0.5);
+        rock.body.collideWorldBounds = true;
+        rock.body.bounce.setTo(1.0);
+        rock.angle = Math.floor(Math.random()*360);
+        game.physics.arcade.velocityFromAngle(rock.angle, 200, rock.body.velocity);
+    }
+    
+    function killRock(rock1, rock2){
+        rock1.kill();
+        rock2.kill();
+        counter+=50
+    }
+    
+    function gameOver(){
+        player.kill();
+        text = game.add.text(game.world.centerX, game.world.centerY, 'Game Over', { font: "64px Arial", fill: "#ffffff", align: "center" });
+        text.anchor.setTo(0.5, 0.5);
+        game.time.events.remove(scoreLoop);
+        game.time.events.remove(rockLoop);
+    }
+    
+    function score() {        
+        counter+=10;
+        text2.setText('Score: ' + counter);
+    }
+//every second, spawn a rock at 426, 30, give random trajectory (180 degree range down and velocity)
+    
+//ship sprite taken from https://opengameart.org/content/spaceship-9
+//rock sprite taken from https://opengameart.org/content/brown-asteroid
